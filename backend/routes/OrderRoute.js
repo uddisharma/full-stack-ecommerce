3d03e6f978d5a6eeeacc9634943bcb5121c09087
Deepak Sharma <112716576+uddisharma@users.noreply.github.com>
2023-03-22 10:17:46 +0530

    Delete backend directory

--- backend/routes/OrderRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-// import adminController from "../controllers/admincontroller.js";
-const orderController = require("../controllers/orderController.js");
-const router = express.Router();
-router.post("/addorder", orderController.SendOrders);
-router.get("/orders", orderController.GetOrders);
-router.delete("/orders/:id", orderController.DeleteOrder);
-
-module.exports = router;
--- backend/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const ContactController = require("../controllers/contactcontroller.js");
-router.post("/contactus", ContactController.SendDetails);
-router.get("/contactus/details", ContactController.getDetails);
-
-module.exports = router;
