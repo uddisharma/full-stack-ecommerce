9fe4a755b9acf358e74483164ab6c78063766a22
Deepak Sharma <112716576+uddisharma@users.noreply.github.com>
2023-03-22 10:20:50 +0530

    Delete frontend directory

--- frontend/public/Mockups/index.js
+++
@@ -1,2 +0,0 @@
-export { default as AcnibetSoap } from "./ACnibetsoap.png";
-export { default as Logo } from "./MitsLogo1.png";
--- frontend/src/Pages/yourOrder.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const yourOrder = () => {
-  return <div>yourOrder</div>;
-};
-
-export default yourOrder;
